6bf6e0775cc2becd0c5e7c67033b01def367a0b3
abaddon-dev <pawel.dwornicki86@gmail.com>
2021-10-09 15:07:09 +0200

    The first level is complete.

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other) {
-        GetComponent<MeshRenderer>().material.color = Color.red;
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit"; 
+        }
     }
 }
